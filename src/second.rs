9416cf04068a11dc1e1c82e8c8fe3a54124bfd2c
Oliver Burt <oliburt@gmail.com>
2021-03-14 21:12:16 +0000

    second list now takes a generic type

--- src/second.rs
+++ src/second.rs
@@ -1,23 +1,23 @@
 /// An ok Singly Linked Stack using Option
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
 // type alias
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-impl List {
+impl<T> List<T> {
     /// Returns a new empty list
     pub fn new() -> Self {
         List { head: None }
     }
 
     /// Pushes a new element onto the stack
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
             next: self.head.take(),
@@ -27,7 +27,7 @@ impl List {
     }
 
     /// Pops the last element off the stack
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -37,7 +37,7 @@ impl List {
 
 // Manual Implementation of drop to avoid "tail recursive" drop implemented
 // by the compiler
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
 
