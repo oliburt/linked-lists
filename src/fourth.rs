e5cfa11191d8bd1e42cce8e6af04f52bacd5846b
Oliver Burt <oliburt@gmail.com>
2021-03-15 21:47:55 +0000

    fourth list, a BAD doubly linked deque, structure and implemented push_front

---
+++ src/fourth.rs
@@ -0,0 +1,64 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// A BAD but safe Doubly-Linked Deque
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    pub fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head -- OK!
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head -- OK
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head -- OK!
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::List;
+    #[test]
+    fn basics() {
+        let list: List<i32> = List::new();
+    }
+}
